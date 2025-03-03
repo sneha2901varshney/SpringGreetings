f7cd06ca891c1d3dd02370daface2762c43e3b41
snehavarshney <snehavarshney111@gmail.com>
2025-03-03 14:50:22 +0530

    Added:Greeting REST APIs

---
+++ springGreeting/src/main/java/com/example/springGreeting/Message.java
@@ -0,0 +1,14 @@
+package com.example.springGreeting;
+
+public class Message {
+    String message;
+    public Message(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ springGreeting/src/test/java/com/example/springGreeting/SpringGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springGreeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
